f8eb5b64abe8d9f80b39be7d62b6a3e2e93f206b
Sahis <sahisneupane23@gmail.com>
2024-12-13 19:55:16 -0800

    feat: Added Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,tsx}", // Scan all JS, TS, JSX, and TSX files in src folder
+  ],
+  theme: {
+    extend: {}, // Add customizations here
+  },
+  plugins: [],
+};
