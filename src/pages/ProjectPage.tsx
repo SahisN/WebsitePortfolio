d7f1b0a7e9d8f79ad8b3647540e324480922dd9c
SahisN <sahisneupane23@gmail.com>
2024-12-19 15:23:51 -0800

    feat: Added Project Cards

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,7 +1,17 @@
+import NavBar from "../components/NavBar";
+import ProjectCard from "../components/ProjectCard";
+
 const ProjectPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">My Projects</h1>
+    <div className="px-[25px] py-[25px] h-screen bg-black">
+    <NavBar pageNo={1} />
+    <div className="py-[20px]">
+      <div className="border-t-2 border-orange-400 w-24 my-4"></div>
+    </div>
+
+      <div className="py-[50px]">
+          <ProjectCard />
+      </div>
     </div>
   );
 };
