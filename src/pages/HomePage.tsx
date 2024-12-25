5163707b0f0536fa6a45060daad0c41a1f132d26
Sahis <sahisneupane23@gmail.com>
2024-12-25 15:33:04 -0800

    added programming language svg

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import NavBar from "../components/NavBar";
-import SocialMedia from "../components/SocialMedia";
 
 const HomePage = () => {
   return (
@@ -9,22 +8,29 @@ const HomePage = () => {
         <div className="border-t-2 border-orange-400 w-24 my-4"></div>
       </div>
 
-      {/** Half text and half image section */}
-      <div className="py-[15px] text-center">
-        <div className="space-y-2 font-mono">
-          <h1 className="text-3xl font-bold text-white">
-            Hi, I'm Sahis Neupane, a
-          </h1>
-          <h1 className="text-3xl font-bold text-white">Front-end Developer</h1>
-          <p className="text-white py-[10px]">
-            I design web apps to turn your ideas into reality
-          </p>
-          <button className="bg-orange-500 text-white rounded-lg h-12 w-32 transition transform hover:scale-110 hover:bg-orange-500 font-bold">
+      <div className="py-[15px]">
+        <div className="space-y-2 font-mono text-center text-white">
+          <h1 className="text-3xl font-bold">Hi, I'm Sahis Neupane, a</h1>
+          <h1 className="text-3xl font-bold">Software Engineer</h1>
+          <div className="py-[15px]">
+            <p>I'm Passionate about building user-friendly solutions</p>
+            <p>and committed to continuous learning and growth while</p>
+            <p>delivering high-quality results.</p>
+          </div>
+
+          <div>
+            {/** Seperator here */}
+            <div className="flex flex-col items-center justify-center">
+              <div className="flex border-t-2 border-grey-400 w-72 my-4 justify-center" />
+            </div>
+            <h2 className="text-2xl font-bold"> Programming Languages I use</h2>
+          </div>
+
+          {/* <button className="bg-orange-500 rounded-lg h-12 w-32 transition transform hover:scale-110 hover:bg-orange-500 font-bold">
             Get in touch
-          </button>
+          </button> */}
         </div>
       </div>
-      <SocialMedia />
     </div>
   );
 };
