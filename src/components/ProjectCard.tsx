a3b45f025db2d9f65083769bb34707cc33a1979c
Sahis <sahisneupane23@gmail.com>
2025-01-12 14:28:34 -0800

    updated projectpage

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,39 +1,45 @@
 interface props {
-  title: string,
-  tools: string[],
-  description: string,
-  image_src: string,
-  link: string,
+  title: string;
+  tools: string[];
+  description: string;
+  image_src: string;
+  link: string;
 }
 
-const redirect = (link : string) => {
-  window.location.href = link
-}
+const redirect = (link: string) => {
+  window.location.href = link;
+};
 
-function ProjectCard({title, tools, description, link} : props) {
+function ProjectCard({ title, tools, description, link }: props) {
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl max-w-[650px] w-auto">
-    <figure>
-      <img
-        src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-        alt="Album" />
-    </figure>
-    <div className="card-body">
-      <h2 className="card-title">{title}</h2>
-      <div className="flex space-x-2">
-        {tools.map((toolName) => (
-          <div className="badge badge-primary">{toolName}</div>
-        ))}
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+          alt="Album"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <div className="flex space-x-2">
+          {tools.map((toolName) => (
+            <div className="badge badge-primary">{toolName}</div>
+          ))}
+        </div>
+        <div className="card-description space-y-4 py-[15px]">
+          <p className="max-w-[300px]">{description}</p>
+          <div>
+            <button
+              className="btn bg-blue-700 text-white hover:bg-blue-600"
+              onClick={() => redirect(link)}
+            >
+              Learn More
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="card-description space-y-6 py-[15px]">
-      <p className="max-w-[300px]">{description}</p>
-      <div>
-        <button className="btn bg-blue-700 text-white hover:bg-blue-600" onClick={() => redirect(link)}>Learn More</button>
-      </div>
-      </div>  
     </div>
-  </div>
-  )
+  );
 }
 
 export default ProjectCard;
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -5,16 +5,21 @@ import { projects } from "../utility/projectData";
 const ProjectPage = () => {
   return (
     <div className="px-[25px] py-[25px] h-screen bg-black">
-    <NavBar pageNo={1} />
-    <div className="py-[20px]">
-      <div className="border-t-2 border-orange-400 w-24 my-4"></div>
-    </div>
+      <NavBar pageNo={1} />
+      <div className="py-[20px]">
+        <div className="border-t-2 border-orange-400 w-24 my-4"></div>
+      </div>
 
-      <div className="py-[30px] grid grid-cols-2 space-y-6">
-          {projects.map((project) => (
-            <ProjectCard title={project.title} description={project.description} tools={project.tools} image_src={project.image} link={project.link}/>
-          ))}
-          
+      <div className="ml-[60px] py-[30px] grid grid-cols-2 gap-6 mx-auto">
+        {projects.map((project) => (
+          <ProjectCard
+            title={project.title}
+            description={project.description}
+            tools={project.tools}
+            image_src={project.image}
+            link={project.link}
+          />
+        ))}
       </div>
     </div>
   );
