758e016dd9f8b10e81ac0fa918ba54cc4a77f16f
Sahis <sahisneupane23@gmail.com>
2025-06-01 22:09:15 -0700

    fix: replace anchor tags with Link components for navigation in MoreProjects and Projects pages

--- src/pages/subpage/Projects.jsx
+++ src/pages/subpage/Projects.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { PROJECTS } from "../../const";
 import { ProjectCard } from "../../widgets/ProjectCard";
 
@@ -21,12 +22,12 @@ export function Projects() {
           ))}
         </div>
         <div className="mt-[30px]">
-          <a
-            href="/sahis-website-portfolio/#projects"
+          <Link
+            to="projects"
             className="px-[10px] py-[10px] justify-center text-center text-white text-xl cursor-pointer rounded-lg max-w-[200px] mx-auto block bg-gradient-to-r from-purple-800 to-purple-900 hover:from-purple-600 hover:to-purple-800 hover:shadow-[0_0_10px_4px_rgba(128,0,128,0.7)] transition-all"
           >
             Check Out More Projects
-          </a>
+          </Link>
         </div>
       </section>
     </div>
